46586beffd1fc063115932e08b17a43a090274ff
carrielin <carrielin@sugarfun.com.tw>
2023-08-13 12:05:27 +0800

    change word

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue 1235" />
+  <HelloWorld msg="Vite + Vue 1asfsfaskkfjl5" />
 </template>
 
 <style scoped>
